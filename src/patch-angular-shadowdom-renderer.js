5cc5fcb90ec593e02c5bcf235cc25827faf78aef
Ryan Bendel <ryan.bendel@autotrader.co.uk>
2025-09-12 13:42:34 +0100

    feat: More reliable way to remove sharedStylesHost from ShadowDomRenderer.
    Previous versions were brittle to changes in the ShadowDomRenderer class signature. This change only removes the sharedStylesHost lines rather than trying to rewrite the class

--- src/patch-angular-shadowdom-renderer.js
+++ src/patch-angular-shadowdom-renderer.js
@@ -5,18 +5,18 @@ const path = require('path');
 
 // Recursively find all dom_renderer*.js and dom_renderer*.mjs files in a directory
 function findDomRendererFiles(dir) {
-  let results = [];
-  const list = fs.readdirSync(dir);
-  list.forEach(file => {
-    const filePath = path.join(dir, file);
-    const stat = fs.statSync(filePath);
-    if (stat && stat.isDirectory()) {
-      results = results.concat(findDomRendererFiles(filePath));
-    } else if (/^dom_renderer.*\.(js|mjs)$/.test(file)) {
-      results.push(filePath);
-    }
-  });
-  return results;
+    let results = [];
+    const list = fs.readdirSync(dir);
+    list.forEach(file => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(findDomRendererFiles(filePath));
+        } else if (/^dom_renderer.*\.(js|mjs)$/.test(file)) {
+            results.push(filePath);
+        }
+    });
+    return results;
 }
 
 const platformBrowserMain = require.resolve('@angular/platform-browser');
@@ -26,25 +26,26 @@ const files = findDomRendererFiles(nodeModulesDir);
 console.log('Found files:', files);
 
 files.forEach(file => {
-  let src = fs.readFileSync(file, 'utf8');
+    let src = fs.readFileSync(file, 'utf8');
 
-  // Only patch the ShadowDomRenderer instantiation in DomRendererFactory2
-  src = src.replace(/(case ViewEncapsulation\.ShadowDom:\s*\n\s*)return new ShadowDomRenderer\(([^)]*)\);/g, (match, p1, args) => {
-    const newArgs = args
-      .split(',')
-      .filter(arg => !/sharedStylesHost/.test(arg))
-      .join(',');
-    return `${p1}return new ShadowDomRenderer(${newArgs});`;
-  });
+    // Only patch the ShadowDomRenderer instantiation in DomRendererFactory2
+    src = src.replace(/(case ViewEncapsulation\.ShadowDom:\s*\n\s*)return new ShadowDomRenderer\(([^)]*)\);/g, (match, p1, args) => {
+        const newArgs = args
+            .split(',')
+            .filter(arg => !/sharedStylesHost/.test(arg))
+            .join(',');
+        return `${p1}return new ShadowDomRenderer(${newArgs});`;
+    });
 
-  src = src.replace(/(class ShadowDomRenderer extends DefaultDomRenderer2\s*\{[\s\S]*?\n\})/m, (match) => {
-    return match
-      .replace(/this\.sharedStylesHost\.addHost\(this\.shadowRoot\);/g, '// this.sharedStylesHost.addHost(this.shadowRoot);')
-      .replace(/this\.sharedStylesHost\.removeHost\(this\.shadowRoot\);/g, '// this.sharedStylesHost.removeHost(this.shadowRoot);');
-  });
+    src = src.replace(/(class ShadowDomRenderer extends DefaultDomRenderer2\s*\{[\s\S]*?\n\})/m, (match) => {
+        return match
+            .replace(/this\.sharedStylesHost = sharedStylesHost;/g, '// this.sharedStylesHost = sharedStylesHost;')
+            .replace(/this\.sharedStylesHost\.addHost\(this\.shadowRoot\);/g, '// this.sharedStylesHost.addHost(this.shadowRoot);')
+            .replace(/this\.sharedStylesHost\.removeHost\(this\.shadowRoot\);/g, '// this.sharedStylesHost.removeHost(this.shadowRoot);');
+    });
 
-  fs.writeFileSync(file, src, 'utf8');
-  console.log(`Patched: ${file}`);
+    fs.writeFileSync(file, src, 'utf8');
+    console.log(`Patched: ${file}`);
 });
 
 console.log('Angular dom_renderer shadowdom patch complete.');
